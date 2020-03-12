b9c30fc640689bd3dbe88c7b592c37c182feae42
Jonas Julian Jensen <jojuljen@gmail.com>
2020-03-12 12:05:54 +0700

    Day 4: Implementing the RTL Module
    Modified:
    * Removed the behavioral model instance
    * Added the RTL debouncer module instance
    * Implemented six test cases

--- debounce/src/debounce_tb.vhd
+++ debounce/src/debounce_tb.vhd
@@ -30,30 +30,12 @@ begin
 
   clk <= not clk after clk_period / 2;
 
-  -- DUT : entity work.debounce(rtl)
-	-- port map (
-	--  clk => clk,
-	--  rst => rst,
-	--  button => button,
-	--  press => press		 
-  -- );
-  
-  DUT : entity work.debounce_model(beh)
-  generic map (
-      -- 0: No error
-      -- 1: Wrong reset value
-      -- 2: No output pulse
-      -- 3: Pulse lasts more than 1 clock cycle
-      -- 4: Accepts button-down < 1000 clock cycles after button-up
-      -- 5: Timeout is slightly longer than 1000 clock cycles
-      -- 6: Continuous button-down produces repeated pulses
-    error_mode => 0
-  )
-  port map (
-    clk => clk,
-    rst => rst,
-    button => button,
-    press => press
+  DUT : entity work.debounce(rtl)
+	port map (
+	 clk => clk,
+	 rst => rst,
+	 button => button,
+	 press => press		 
   );
 
   SEQUENCER_PROC : process
@@ -72,19 +54,45 @@ begin
     wait for 2 * clk_period;
     rst <= '0';
 
-    pulse;
-
     -- Check the value after reset
+    assert press = '0'
+      report "should be '0' after reset"
+      severity failure;
 
     -- Check value after the first button press
+    pulse;
+    assert press = '1'
+      report "press should be '1' after first button-down"
+      severity failure;
 
     -- Check that the pulse lasts for 1 clock cycle
-
+    wait until rising_edge(clk);
+    assert press = '0'
+      report "pulse should last for 1 clock cycle"
+      severity failure;
+    
     -- Check that a button-down < 1000 clock cycles after button-up is ignored
+    wait for 996 * clk_period;
+    pulse;
+    assert press = '0'
+      report "button-down < 1000 cycles after button-up should be ignored"
+      severity failure;
+    wait for 1 * clk_period;
 
     -- Check that a button-down > 1000 cycles after the last button-up is counted
-
+    wait for 1000 * clk_period;
+    pulse;
+    assert press = '1'
+      report "button-down 1000 cycles after button-up should be counted"
+      severity failure;
+    
     -- Check that there is no new pulse on continuous button-down
+    button <= '1'; -- hold down the button
+    wait for 3 * 1000 * clk_period;
+    assert press = '0' report "should be '0' after long button-down";
+    assert press'stable(3 * 1000 * clk_period)
+      report "should remain '0' during long button-down"
+      severity failure;
 
     wait for 10 * clk_period;
     write(str, string'("Test finished: OK"));
