485fe9d09f35670435ebd352571842247be0c69b
Jonas Julian Jensen <jojuljen@gmail.com>
2020-03-13 14:49:40 +0700

    Day 5: Let's Wrap This Up
    Modified:
    * Implemented the debounce RTL module

--- debounce/src/debounce_rtl.vhd
+++ debounce/src/debounce_rtl.vhd
@@ -9,11 +9,43 @@ entity debounce is
     button : in std_logic;
     press : out std_logic
   );
-end debounce;
+end debounce; 
 
 architecture rtl of debounce is
+
+  signal timeout : integer range 0 to 999;
+  signal button_p1 : std_logic;
+
 begin
+  
+  DEBOUNCE_PROC : process(clk)
+  begin
+    if rising_edge(clk) then
+      if rst = '1' then
+        timeout <= 0;
+        press <= '0';
+        button_p1 <= '0';
+        
+      else
+        press <= '0';
+        button_p1 <= button;
 
+        if timeout = 0 then
 
+          if button = '1' and button_p1 = '0' then
+            press <= '1';
+          end if;
+
+        else
+          timeout <= timeout - 1;
+        end if;
+
+        if button = '0' and button_p1 = '1' then
+          timeout <= 999;
+        end if;
+
+      end if;
+    end if;
+  end process;
 
 end architecture;
